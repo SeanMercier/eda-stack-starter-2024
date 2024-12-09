122afb7110825fedc2a1af047b07f79f4f3ee731
seanm <20094677@mail.wit.ie>
2024-12-09 10:46:51 +0000

    Function to search for matching transporters.

--- env.ts
+++ env.ts
@@ -1,3 +1,3 @@
 export const SES_REGION = 'eu-west-1';
-export const SES_EMAIL_FROM = 'your verified email';
-export const SES_EMAIL_TO = 'your verified email';
+export const SES_EMAIL_FROM = '20094677@mail.wit.ie';
+export const SES_EMAIL_TO = '20094677@mail.wit.ie';
--- question1/answer.ts
+++ question1/answer.ts
@@ -1,1 +1,61 @@
-console.log('Hello world')
+interface Car {
+    make: string;
+    color: string;
+    registration: string;
+    owner: string;
+  }
+  
+  interface Bicycle {
+    make: string;
+    color: string;
+    owner: string;
+  }
+  
+  type Transporter = Car | Bicycle;
+  
+  const database: Transporter[] = [
+    {
+      make: "Toyota Yaris",
+      color: "Red",
+      registration: "231WD1234",
+      owner: "Jane Smith",
+    },
+    {
+      make: "Suzuki Swift",
+      color: "Blue",
+      registration: "241WD4321",
+      owner: "Paul O Regan",
+    },
+    {
+      make: "Ford Puma",
+      color: "Blue",
+      registration: "241WD1212",
+      owner: "Eileen Silk",
+    },
+    {
+      make: "Revel Rascal XO",
+      color: "Blue",
+      owner: "Cindy Tamoka",
+    },
+    {
+      make: "Yeti SB140 LR",
+      color: "Red",
+      owner: " ",
+    },
+  ];
+
+  function getMatches(criteria: (t: Transporter) => boolean): Transporter[] {
+    return database.filter(criteria);
+  }
+  
+  // Get Blue transporters
+  const blueTransporters = getMatches((t) => t.color === "Blue");
+  console.log("Blue Transporters:", blueTransporters);
+  
+  // Get Red cars with a registration
+  const redCars = getMatches((t) => t.color === "Red" && "registration" in t);
+  console.log("Red Cars:", redCars)
+
+
+
+  
